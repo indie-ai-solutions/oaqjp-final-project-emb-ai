495d6f070839f638ff123da079cfcf2765fff48a
indie-ai-solutions <ngeorge@indieaisolutions.com>
2026-05-16 00:22:00 -0400

    Task 6

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,10 +7,9 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json = myobj, headers=header)
 
     formatted_response = json.loads(response.text)
-    # emotions = formatted_response['emotionPredictions'][0]['emotion']
-    # 
-    # dominant_emotion = max(emotions, key=emotions.get)
-    # emotions["dominant_emotion"] = dominant_emotion
-# 
-    # return emotions
-    return formatted_response
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    
+    dominant_emotion = max(emotions, key=emotions.get)
+    emotions["dominant_emotion"] = dominant_emotion
+   
+    return emotions
--- server.py
+++ server.py
@@ -11,15 +11,21 @@ def render_index_page():
 
 @app.route("/emotionDetector")
 def emote():
-    text_to_analyse = request.args.get('textToAnalyse')
-    
-    return text_to_analyse
+    print(request.args)
+    print(request.args.get('textToAnalyze'))
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
 
-    # response = emotion_detector(text_to_analyse)
+    strResponse = (
+        "For the given statement, the system response is 'anger': " + str(response['anger']) + ", "
+        "'disgust': " + str(response['disgust']) + ", "
+        "'fear': " + str(response['fear']) + ", "
+        "'joy': " + str(response['joy']) + ", "
+        "and 'sadness': " + str(response['sadness']) + ". "
+        "The dominant emotion is " + response['dominant_emotion'] + "."
+    )
 
-    # s = "For the given statement, the system response is 'anger': " + str(response['anger'])
-
-    # return response
+    return strResponse
 
 if __name__ == "__main__":
         app.run(host="0.0.0.0", port=5000)
